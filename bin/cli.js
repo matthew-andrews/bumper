c23ff1d485278eafe5c3982022380108609d3a1e
Matt Andrews <matt@mattandre.ws>
2015-05-01 09:53:25 +0100

    Update cli.js

--- bin/cli.js
+++ bin/cli.js
@@ -25,7 +25,7 @@ function setUpCommand(level) {
 				dryRun: opts.dryRun
 			})
 				.then(function(annotation) {
-					process.stdout.write(annotation + '\n');
+					console.log(annotation);
 				})
 				.catch(function(err) {
 					logger.error(err);
